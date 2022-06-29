ac5831cdd96f63e86afed1e495d1d3ebef1dda42
jessamynr2020 <jessamynrodriguez@yahoo.com>
2022-06-28 23:32:56 -0500

    mobile view updated

--- app.js
+++ app.js
@@ -1,37 +1,45 @@
-document.querySelectorAll(".carousel").forEach((carousel) => {
-    const items = carousel.querySelectorAll(".carousel__item");
-    const buttonsHtml = Array.from(items, () => {
+document.querySelectorAll('.carousel').forEach((carousel) => {
+  const items = carousel.querySelectorAll('.carousel__item');
+  const buttonsHtml = Array.from(items, () => {
       return `<span class="carousel__btn">&#10007;</span>`;
-    });
+});
   
-    carousel.insertAdjacentHTML(
-      "beforeend",
-      `
-          <div class="carousel__nav">
-              ${buttonsHtml.join("")}
-          </div>
-      `
-    );
+carousel.insertAdjacentHTML(
+  "beforeend",
+    `
+      <div class="carousel__nav">
+        ${buttonsHtml.join("")}
+      </div>
+    `
+);
   
-    const buttons = carousel.querySelectorAll(".carousel__btn");
+const buttons = carousel.querySelectorAll('.carousel__btn');
   
-    buttons.forEach((button, i) => {
-      button.addEventListener("click", () => {
-        // un-select all the items
-        items.forEach((item) =>
-          item.classList.remove("carousel__item--selected")
+buttons.forEach((button, i) => {
+  button.addEventListener("click", () => {
+    items.forEach((item) =>
+      item.classList.remove('carousel__item--selected')
         );
-        buttons.forEach((button) =>
-          button.classList.remove("carousel__btn--selected")
+    buttons.forEach((button) =>
+          button.classList.remove('carousel__btn--selected')
         );
-  
-        items[i].classList.add("carousel__item--selected");
-        button.classList.add("carousel__btn--selected");
-      });
-    });
-  
-    // Select the first item on page load
-    items[0].classList.add("carousel__item--selected");
-    buttons[0].classList.add("carousel__btn--selected");
+    items[i].classList.add('carousel__item--selected');
+    button.classList.add('carousel__btn--selected');
   });
+});
   
+    items[0].classList.add('carousel__item--selected');
+    buttons[0].classList.add('carousel__btn--selected');
+  });
+
+const menu = document.querySelector('#mobile__menu');
+const menuLinks = document.querySelector('.navbar__menu');
+const body = document.querySelector('body');
+
+const mobileMenu = () => {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+  body.classList.toggle('active');
+};
+
+menu.addEventListener('click', mobileMenu);
